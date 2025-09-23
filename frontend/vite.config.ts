1f4fe5af82356f74545176dbc9d4ba381ac016af
Adeola Oni <oni.adeola524@gmail.com>
2025-09-23 14:03:39 -0800

    built frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          ui: ["@radix-ui/react-dropdown-menu", "@radix-ui/react-dialog"],
+        },
+      },
+    },
+
+    chunkSizeWarningLimit: 1000,
+  },
 });
