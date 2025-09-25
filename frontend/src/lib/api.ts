1342561ed1b749011fb7bd8bb43ce130f3d3aa53
charming0524 <oni.adeola@yahoo.com>
2025-09-24 17:43:40 -0800

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,16 +3,23 @@ import Cookies from "js-cookie";
 import constant from "./constant";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5500/api", // Setup from .env
-  timeout: 10000, // 10s timeout
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  withCredentials: true, 
 });
 
+
 api.interceptors.request.use((config) => {
-  const accessToken = Cookies.get(constant.ACCESS_TOKEN_KEY);
+ 
+  let accessToken = Cookies.get(constant.ACCESS_TOKEN_KEY);
+
+ 
+  if (!accessToken) {
+    accessToken = localStorage.getItem(constant.ACCESS_TOKEN_KEY) || "";
+  }
 
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
