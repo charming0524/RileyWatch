8054db99f6c973c27817494febfc78a548e179f8
charming0524 <oni.adeola@yahoo.com>
2025-10-02 08:27:27 -0800

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,7 +4,7 @@ import constant from "./constant";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
